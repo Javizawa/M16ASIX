a204c7d6c3e46a556381131cceb7b7f27cefd6b0
MONLAU\salvadorj <Javier>
2022-12-13 12:20:35 +0100

    My fist game

---
+++ Ejercicio1/Ejercicio1.cpp
@@ -0,0 +1,59 @@
+// Ejercicio1.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+using namespace std;
+
+//Atributos del enemigo
+int enemyHP = 100;
+int enemyDmg = 500;
+string enemyName = "Sauron";
+bool enemyIsAlive = true;
+
+//Atributos del personaje
+int heroHP = 200;
+int heroDamage;
+string heroName;
+bool heroIsAlive = true;
+
+
+void gameStart() {
+    cout << "El enemigo " << enemyName << " ha aparecido\n";
+    cout << "Como se llama el heroe?\n";
+    cin >> heroName;
+    cout << "El nombre del heroe es " << heroName << "\n";
+}
+
+int heroAttack() {
+    return enemyHP - heroDamage;
+}
+
+
+bool checkEnemyStatus() {
+    if (enemyHP <= 0) {
+        cout << "Te has cargado al enemigo " << enemyName << " \n";
+        enemyHP = 0;
+        return false;
+        //enemyIsAlive = false;
+    }
+    else {
+        cout << "El enemigo " << enemyName << " tiene " << enemyHP << " puntos de vida\n";
+        return true;
+    }
+}
+
+int main()
+{
+    gameStart();
+    while(enemyIsAlive && heroIsAlive){
+            cout << "Cuanto danyo quieres hacer al enemigo " << enemyName <<"?\n";
+            cin >> heroDamage;
+            enemyHP = heroAttack(); 
+            if (checkEnemyStatus()) {
+                heroHP = heroHP - enemyDmg;
+                cout << "El enemigo " << enemyName << " te ha atacado y te quedan " << heroHP << " puntos de vida\n";
+            }
+
+    }
+}
